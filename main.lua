3233006d094cdbed1e618703bc39cc977272efe1
xXsigmagyatrizzlerXDXx <ihatemywifeihatemywife@gmail.com>
2024-07-06 16:27:20 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -137,19 +137,19 @@ xpcall(function()
 	}
 	
 	local DontSave = {
-		{'Parent'},
-		{'BrickColor'},
+		{'Parent', {}},
+		{'BrickColor', {}},
 		{'Orientation', {"ParticleEmitter"}},
-		{'Position'},
-		{"WorldCFrame"},
-		{"WorldPosition"},
-		{'WorldPivot'},
-		{"Grip"},
-		{"Origin"},
-		{"PrimaryPart"},
-		{"UniqueId"},
-		{"PivotOffset"};
-		{"Pivot Offset"};
+		{'Position', {}},
+		{"WorldCFrame", {}},
+		{"WorldPosition", {}},
+		{'WorldPivot', {}},
+		{"Grip", {}},
+		{"Origin", {}},
+		{"PrimaryPart", {}},
+		{"UniqueId", {}},
+		{"PivotOffset", {}};
+		{"Pivot Offset", {}};
 	}
 
 	local DontSaveIf = {
@@ -166,7 +166,7 @@ xpcall(function()
 		
 		for _, disallowedproperty in DontSave do
 			if disallowedproperty[1] == property then
-				if not table.find(DontSave, objectClass) then
+				if #disallowedproperty > 1 and not table.find(disallowedproperty[2], objectClass) then
 					disallow = true
 					break
 				end
